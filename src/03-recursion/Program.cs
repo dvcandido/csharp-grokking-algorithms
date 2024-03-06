77dbd91860de572e926676d4c7bc17b51a0583eb
Dougals Vaz Candido <douglasvaz@hotmail.com>
2024-03-06 17:15:02 -0300

    Add Cap 03 Recursion

---
+++ src/03-recursion/Program.cs
@@ -0,0 +1,11 @@
+﻿int val = 3;
+
+Console.WriteLine($"The factorial of {val} is {fat(val)}");
+
+int fat(int val)
+{
+    if (val == 1)
+        return 1;
+    else
+        return val * fat(val-1);
+}
